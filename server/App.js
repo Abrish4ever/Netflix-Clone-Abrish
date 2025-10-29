fb28177fdafcfa40bba848d4744eabf111251346
Abrish <abrish4ever2015@gmail.com>
2025-10-29 17:09:33 +0300

    Add comment to clarify token security in backend

--- server/App.js
+++ server/App.js
@@ -7,6 +7,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
+//this Used to secure the token in backend
 // Endpoint to send the token to frontend
 app.get("/api/token", (req, res) => {
   res.json({ token: process.env.TMDB_TOKEN });
